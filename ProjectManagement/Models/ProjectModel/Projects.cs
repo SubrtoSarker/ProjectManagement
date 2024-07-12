3910dc1f4dfd407f38ba56cc431bb5dfa54ae1f5
Subrato_Sarke <supersubrato@gmail.com>
2024-07-12 23:07:38 +0600

    1234

--- ProjectManagement/Models/ProjectModel/Projects.cs
+++ ProjectManagement/Models/ProjectModel/Projects.cs
@@ -1,11 +1,19 @@
-﻿namespace ProjectManagement.Models.ProjectModel
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectManagement.Models.ProjectModel
 {
     public class Projects
     {
+        [Display(Name = "Project ID")]
         public int IntProjectID { get; set; }
+        [Display(Name = "Name")]
         public string StrProjectName { get; set; }
+        [Display(Name = "Compleate")]
         public bool IsCompleate { get; set; }
+        [Display(Name = "Running Days")]
         public int DaysPassed { get; set; }
+        [Display(Name = "Total User")]
         public int UserCount { get; set; }
     }
 }
