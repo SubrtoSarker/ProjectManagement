e2136a4121c7ddef8a2155c8d6f3601f01923158
Subrato_Sarke <supersubrato@gmail.com>
2024-08-07 23:19:46 +0600

    Add Personal Report

--- ProjectManagement/Components/Shared/MainLayout.razor.cs
+++ ProjectManagement/Components/Shared/MainLayout.razor.cs
@@ -40,6 +40,7 @@ namespace ProjectManagement.Components.Shared
             {
                 new() { Text = "Home", Icon = "fa-solid fa-fw fa-home", Url = "/Home" , Match = NavLinkMatch.All},
                 new() { Text = "Tasks", Icon = "fa-solid fa-fw fa-tasks", Url = "/Task" },
+                new() { Text = "Task Report", Icon = "fa-solid fa-fw fa-chart-line", Url = "/TaskReport" },
                 new() { Text = "Project", Icon = "fa-solid fa-fw fa-project-diagram", Url = "/Project" },
                 new() { Text = "Users", Icon = "fa-solid fa-fw fa-users-cog", Url = "/User" },
 
@@ -58,8 +59,7 @@ namespace ProjectManagement.Components.Shared
             {
                 new() { Text = "Home", Icon = "fa-solid fa-fw fa-home", Url = "/Home", Match = NavLinkMatch.All },
                 new() { Text = "Tasks", Icon = "fa-solid fa-fw fa-tasks", Url = "/Task" },
-
-
+                new() { Text = "Task Report", Icon = "fa-solid fa-fw fa-chart-line", Url = "/TaskReport" },
             };
 
             if (displayAccount == "All")
