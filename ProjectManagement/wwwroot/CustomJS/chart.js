58cd628d3830728ed8e98cee9baa115bffeb0f22
Subrato_Sarke <supersubrato@gmail.com>
2024-08-10 23:13:11 +0600

    Header

--- ProjectManagement/wwwroot/CustomJS/chart.js
+++ ProjectManagement/wwwroot/CustomJS/chart.js
@@ -21,6 +21,30 @@ function createDonutChart(data) {
 
         series.data.setAll(chartData);
 
+        // Add labels
+        series.slices.template.events.on("hit", function (ev) {
+            var slice = ev.target;
+            var dataItem = slice.dataItem;
+            var title = dataItem.category;
+            var value = dataItem.value;
+
+            // Display custom labels or headers
+            console.log("Task:", title, "Percent:", value, "Work Time:", value); // You can customize this
+        });
+
+        // Add legend
+        var legend = chart.children.push(am5.Legend.new(root, {
+            position: "right",
+            marginRight: 15
+        }));
+
+        legend.data.setAll(series.dataItems);
+
+        // Configure tooltips
+        series.slices.template.set("tooltipText", "{category}: {value}%");
+        series.slices.template.set("tooltipY", 0);
+        series.slices.template.set("tooltipX", 0);
+
         series.appear(1000, 100);
     });
 }
