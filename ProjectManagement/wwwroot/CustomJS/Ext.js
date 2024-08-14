0d8314c48b156a364e1532a4df8af70d795cb906
Subrato_Sarke <supersubrato@gmail.com>
2024-08-14 21:21:56 +0600

    Print

---
+++ ProjectManagement/wwwroot/CustomJS/Ext.js
@@ -0,0 +1,10 @@
+﻿function printDiv(divId) {
+    var divToPrint = document.getElementById(divId);
+    var newWindow = window.open('', '', 'height=600,width=800');
+    newWindow.document.write('<html><head><title>Print</title>');
+    newWindow.document.write('</head><body >');
+    newWindow.document.write(divToPrint.outerHTML);
+    newWindow.document.write('</body></html>');
+    newWindow.document.close();
+    newWindow.print();
+}
